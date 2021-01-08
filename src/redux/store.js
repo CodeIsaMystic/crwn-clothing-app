a60af4aeb70fef61914aea0f6928431179cad38d
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-08 07:28:11 +0100

    implemented directory selector, reducer for the shop data home page on redux/directory

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -7,7 +8,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+
+
+export default { store, persistor };
