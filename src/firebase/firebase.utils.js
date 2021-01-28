30c8c9e47c2f7d1ff15247a0bd0c12c856ea71b0
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-28 18:18:11 +0100

    added shop data to redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -97,11 +97,12 @@ export const convertCollectionSnapshotToMap = (collections) => {
   });
 
 
-  console.log(transformedCollection);
-  /*return transformedCollection.reduce((accumulator, collection) => {
+  /*console.log(transformedCollection);*/
+
+  return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
-  }, {});*/
+  }, {});
 };
 
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 
 
@@ -12,10 +16,14 @@ class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot => {
-      convertCollectionSnapshotToMap(snapshot);
+
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      /*console.log(collectionsMap);*/
+      updateCollections(collectionsMap);
     })
   }
 
@@ -31,7 +39,13 @@ class ShopPage extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
 
 
 
-export default ShopPage;
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
